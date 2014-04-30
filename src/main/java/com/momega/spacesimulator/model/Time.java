a71b1858f46fe81b54d9ce3300ccbe2012cb953c
Martin Vanek <mvanek@email.cz>
2014-04-30 07:39:04 +0200

    Time Added

---
+++ src/main/java/com/momega/spacesimulator/model/Time.java
@@ -0,0 +1,7 @@
+package com.momega.spacesimulator.model;
+
+/**
+ * Created by martin on 4/29/14.
+ */
+public class Time {
+}
---
+++ src/main/java/com/momega/spacesimulator/renderer/TimeRenderer.java
@@ -0,0 +1,7 @@
+package com.momega.spacesimulator.renderer;
+
+/**
+ * Created by martin on 4/30/14.
+ */
+public class TimeRenderer {
+}
