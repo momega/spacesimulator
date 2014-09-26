360af4d8b7ec20542b3c9ecafddeaf68ea1d9da3
Martin Vanek <mvanek@email.cz>
2014-09-26 12:45:25 +0200

    testing vbo implementation

--- src/test/java/com/momega/spacesimulator/testing/TestingGLRenderer.java
+++ src/test/java/com/momega/spacesimulator/testing/TestingGLRenderer.java
@@ -3,6 +3,9 @@
  */
 package com.momega.spacesimulator.testing;
 
+import java.nio.FloatBuffer;
+
+import javax.media.opengl.GL;
 import javax.media.opengl.GL2;
 import javax.media.opengl.GLAutoDrawable;
 import javax.media.opengl.glu.GLU;
@@ -10,6 +13,7 @@ import javax.media.opengl.glu.GLU;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.jogamp.common.nio.Buffers;
 import com.momega.spacesimulator.model.Camera;
 import com.momega.spacesimulator.model.Vector3d;
 import com.momega.spacesimulator.opengl.AbstractGLRenderer;
@@ -28,6 +32,18 @@ public class TestingGLRenderer extends AbstractGLRenderer {
 
 	private CompositeRenderer renderer;
 
+	private int vbo_vertex_handle;
+
+	private int vbo_color_handle;
+
+	private int vertex_size;
+
+	private int color_size;
+
+	private int vertices;
+
+	private int[] handles;
+
 	public TestingGLRenderer(Camera camera, CompositeRenderer renderer) {
 		super();
 		this.camera = camera;
@@ -36,8 +52,50 @@ public class TestingGLRenderer extends AbstractGLRenderer {
 
 	@Override
 	protected void init(GL2 gl) {
-		 glu = new GLU();
-		 renderer.init(gl);
+		glu = new GLU();
+		 
+		boolean extensionOK = gl.isExtensionAvailable
+					("GL_ARB_vertex_buffer_object");
+		System.out.println( "VBO extension: "+extensionOK ); 
+				      
+	    boolean functionsOK = 
+	    			gl.isFunctionAvailable("glGenBuffersARB") &&
+	    			gl.isFunctionAvailable("glBindBufferARB") &&
+	    			gl.isFunctionAvailable("glBufferDataARB") &&
+	    			gl.isFunctionAvailable("glDeleteBuffersARB");      
+	    System.out.println( "Functions: "+ functionsOK); 
+	    
+	    vertices = 3;
+
+	    vertex_size = 3;
+	    color_size = 3;
+
+	    FloatBuffer vertex_data = Buffers.newDirectFloatBuffer(vertices * vertex_size);
+	    vertex_data.put(new float[] { -1f, -1f, 0f, });
+	    vertex_data.put(new float[] { 1f, -1f, 0f, });
+	    vertex_data.put(new float[] { 1f, 1f, 0f, });
+	    vertex_data.flip();
+
+	    FloatBuffer color_data = Buffers.newDirectFloatBuffer(vertices * color_size);
+	    color_data.put(new float[] { 1f, 0f, 0f, });
+	    color_data.put(new float[] { 0f, 1f, 0f, });
+	    color_data.put(new float[] { 0f, 0f, 1f, });
+	    color_data.flip();
+	    
+	    handles = new int[2];
+	    gl.glGenBuffers(2, handles, 0);
+	    vbo_vertex_handle = handles[0];
+	    
+	    gl.glBindBuffer(GL.GL_ARRAY_BUFFER, vbo_vertex_handle);
+	    gl.glBufferData(GL.GL_ARRAY_BUFFER, Buffers.SIZEOF_FLOAT * vertices * vertex_size, vertex_data, GL.GL_STATIC_DRAW);
+	    gl.glBindBuffer(GL.GL_ARRAY_BUFFER, 0);
+
+	    vbo_color_handle = handles[1];
+	    gl.glBindBuffer(GL.GL_ARRAY_BUFFER, vbo_color_handle);
+	    gl.glBufferData(GL.GL_ARRAY_BUFFER, Buffers.SIZEOF_FLOAT * vertices * color_size, color_data, GL.GL_STATIC_DRAW);
+	    gl.glBindBuffer(GL.GL_ARRAY_BUFFER, 0);
+		 
+		renderer.init(gl);
 	}
 	
 
@@ -50,16 +108,32 @@ public class TestingGLRenderer extends AbstractGLRenderer {
 	protected void draw(GLAutoDrawable drawable) {
 		GL2 gl = drawable.getGL().getGL2(); 
 
-      gl.glBegin(GL2.GL_TRIANGLE_STRIP); // draw using triangles
-      gl.glColor3f( 1,0,0);
-      gl.glVertex3f(0.0f, 10.0f, 0.0f);
-      gl.glColor3f( 0,1,0);
-      gl.glVertex3f(-10.0f, -10.0f, 0.0f);
-      gl.glColor3f( 0,0,1);
-      gl.glVertex3f(10.0f, -10.0f, 0.0f);
-      gl.glEnd();
+//      gl.glBegin(GL2.GL_TRIANGLE_STRIP); // draw using triangles
+//      gl.glColor3f( 1,0,0);
+//      gl.glVertex3f(0.0f, 10.0f, 0.0f);
+//      gl.glColor3f( 0,1,0);
+//      gl.glVertex3f(-10.0f, -10.0f, 0.0f);
+//      gl.glColor3f( 0,0,1);
+//      gl.glVertex3f(10.0f, -10.0f, 0.0f);
+//      gl.glEnd();
+		
+		gl.glBindBuffer(GL2.GL_ARRAY_BUFFER, vbo_vertex_handle);
+		gl.glVertexPointer(vertex_size, GL2.GL_FLOAT, 0, 0l);
+
+		gl.glBindBuffer(GL2.GL_ARRAY_BUFFER, vbo_color_handle);
+		gl.glColorPointer(color_size, GL2.GL_FLOAT, 0, 0l);
+
+		gl.glEnableClientState(GL2.GL_VERTEX_ARRAY);
+		gl.glEnableClientState(GL2.GL_COLOR_ARRAY);
+
+		gl.glDrawArrays(GL2.GL_TRIANGLES, 0, vertices);
+
+		gl.glDisableClientState(GL2.GL_COLOR_ARRAY);
+		gl.glDisableClientState(GL2.GL_VERTEX_ARRAY);
       
       renderer.draw(drawable);
+      
+      //logger.info("draw");
 	}
 
 	@Override
@@ -84,6 +158,9 @@ public class TestingGLRenderer extends AbstractGLRenderer {
 	
 	@Override
 	protected void dispose(GL2 gl) {
+		
+		gl.glDeleteBuffers(2, handles, 0);
+		
 		renderer.dispose(gl);
 		super.dispose(gl);
 	}
