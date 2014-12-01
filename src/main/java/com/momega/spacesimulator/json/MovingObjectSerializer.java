c60f94fe7634f4bdbfafdf0c361b179aa59dade0
Martin Vanek <mvanek@email.cz>
2014-12-01 01:22:21 +0100

    orbital point save test fixed

--- src/main/java/com/momega/spacesimulator/json/MovingObjectSerializer.java
+++ src/main/java/com/momega/spacesimulator/json/MovingObjectSerializer.java
@@ -6,6 +6,8 @@ package com.momega.spacesimulator.json;
 import org.springframework.stereotype.Component;
 
 import com.google.gson.JsonObject;
+import com.momega.spacesimulator.model.Apsis;
+import com.momega.spacesimulator.model.KeplerianTrajectory;
 import com.momega.spacesimulator.model.MovingObject;
 import com.momega.spacesimulator.model.UserOrbitalPoint;
 
@@ -30,6 +32,17 @@ public class MovingObjectSerializer extends AbstractSerializer<MovingObject> {
 		// relink orbital points
 		for(UserOrbitalPoint userOrbitalPoint : value.getUserOrbitalPoints()) {
 			userOrbitalPoint.setMovingObject(value);
+		}
+		KeplerianTrajectory trajectory = value.getTrajectory();
+		if (trajectory != null) {
+			Apsis apsis = trajectory.getApoapsis();
+			if (apsis != null) {
+				apsis.setMovingObject(value);
+			}
+			apsis = trajectory.getPeriapsis();
+			if (apsis != null) {
+				apsis.setMovingObject(value);
+			}
 		}
 	}
 	
---
+++ src/main/java/com/momega/spacesimulator/json/SpacecraftSerializer.java
@@ -0,0 +1,42 @@
+package com.momega.spacesimulator.json;
+
+import org.springframework.stereotype.Component;
+
+import com.google.gson.JsonObject;
+import com.momega.spacesimulator.model.Maneuver;
+import com.momega.spacesimulator.model.OrbitIntersection;
+import com.momega.spacesimulator.model.Spacecraft;
+import com.momega.spacesimulator.model.Target;
+
+@Component
+public class SpacecraftSerializer extends AbstractSerializer<Spacecraft> {
+
+	public SpacecraftSerializer() {
+		super(Spacecraft.class);
+	}
+
+	@Override
+	public void write(JsonObject object, Spacecraft value) {
+		// do nothing
+	}
+
+	@Override
+	public void read(JsonObject object, Spacecraft value) {
+		for(Maneuver maneuver : value.getManeuvers()) {
+			maneuver.getStart().setMovingObject(value);
+			maneuver.getEnd().setMovingObject(value);
+		}
+		Target target = value.getTarget();
+		if (target != null) {
+			for(OrbitIntersection oi : target.getOrbitIntersections()) {
+				oi.setMovingObject(value);
+			}
+		}
+	}
+	
+	@Override
+	public Class<?> getClass(JsonObject object) {
+		return null;
+	}
+
+}
