ad5f0bb7abc9f385f697a8b44f1aaf793055c40e
Martin Vanek <mvanek@email.cz>
2015-02-13 07:54:57 +0100

    builders added

--- web/src/main/webapp/project-controller.js
+++ web/src/main/webapp/project-controller.js
@@ -4,6 +4,8 @@ spaceSimulatorApp.controller('ProjectController', ['$scope', function($scope) {
 
 	$scope.projects = [{name: 'Mars Mission', id: '1'}, {name: 'Moon Mission', id: '2'}];
 	
+	$scope.builders = [{name: 'Mars Mission', id: '1'}, {name: 'Moon Mission', id: '2'}];
+	
 }]);
 
 spaceSimulatorApp.controller('HelpController', ['$scope', function($scope) {
